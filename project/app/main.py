2709ad4de1a73d617723bdd19bb2eb8d74faaff7
effer <effernandezr@gmail.com>
2021-05-19 13:44:50 -0400

    Remove unused dependencies

--- project/app/main.py
+++ project/app/main.py
@@ -2,7 +2,7 @@ import logging
 
 from fastapi import FastAPI
 
-from app.api import ping, summaries
+from app.api import summaries
 from app.db import init_db
 
 log = logging.getLogger("uvicorn")
@@ -10,7 +10,6 @@ log = logging.getLogger("uvicorn")
 
 def create_application() -> FastAPI:
     application = FastAPI()
-    application.include_router(ping.router)
     application.include_router(
         summaries.router, prefix="/summaries", tags=["summaries"]
     )
